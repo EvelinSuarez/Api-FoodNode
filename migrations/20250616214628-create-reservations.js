ad0f57300ba4d54f089fb6d302f35daf46fcff1b
EvelinSuarez <127216080+EvelinSuarez@users.noreply.github.com>
2025-06-16 18:31:13 -0500

    correccion en migrations

--- migrations/20250616214628-create-reservations.js
+++ migrations/20250616214628-create-reservations.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Reservations', {
+    await queryInterface.createTable('reservations', {
       idReservations: {
         type: Sequelize.INTEGER,
         primaryKey: true,
@@ -73,6 +73,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reservations');
+    await queryInterface.dropTable('reservations');
   }
 };
